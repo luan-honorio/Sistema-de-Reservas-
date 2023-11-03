8063ef4fc0c2e80b276408c6fb51aeb1f3abccb0
luan-honorio <honorioluan9@gmail.com>
2023-11-03 12:31:19 -0300

    web Api

---
+++ api_webSpecialEdition/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api_webSpecialEdition.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ api_webSpecialEdition/Controllers/ItemsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api_webSpecialEdition;
+
+namespace api_webSpecialEdition.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private Session3Entities db = new Session3Entities();
+
+        // GET: api/Items
+        public IQueryable<Item> GetItems()
+        {
+            return db.Items;
+        }
+
+        // GET: api/Items/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult GetItem(long id)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/Items/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutItem(long id, Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Items
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult PostItem(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Items.Add(item);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.ID }, item);
+        }
+
+        // DELETE: api/Items/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult DeleteItem(long id)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            db.Items.Remove(item);
+            db.SaveChanges();
+
+            return Ok(item);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ItemExists(long id)
+        {
+            return db.Items.Count(e => e.ID == id) > 0;
+        }
+
+
+        [ResponseType(typeof(List<Item>))]
+        [Route("Itens/Areas/{Nome}/{data}")]
+        [HttpGet]
+        public IHttpActionResult GetItemName(string Nome, string data)
+        {
+            DateTime DateUse = Convert.ToDateTime(data);
+
+            List<Item> item = db.Items
+                .Where(x => x.Area.Name.StartsWith(Nome))
+                .Where(d => d.ItemPrices.Any(DATA => DATA.Date == DateUse ))
+                .ToList();        
+                return Ok(item);
+           
+
+   
+        
+        
+        }
+    
+    }
+}
